01a5b7b737a7be5e7347c5772956f22245882246
sfx <stefanfranke@stefanfranke.com>
2015-04-21 23:15:52 +0200

    Initial commit. Just a bunch of screens (canvases) and buttons to flip between them.

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+
+	public GameObject titleScreen;
+	public GameObject optionsScreen;
+	public GameObject highScoreScreen;
+
+	public void NavigateToOptions() {
+		this.titleScreen.SetActive(false);
+		this.optionsScreen.SetActive(true);
+		this.highScoreScreen.SetActive(false);
+	}
+
+	public void NavigateToHighScores() {
+		this.titleScreen.SetActive(false);
+		this.optionsScreen.SetActive(false);
+		this.highScoreScreen.SetActive(true);
+	}
+
+	public void NavigateToTitle() {
+		this.titleScreen.SetActive(true);
+		this.optionsScreen.SetActive(false);
+		this.highScoreScreen.SetActive(false);	
+	}
+
+	// Use this for initialization
+	//void Start () {	
+	//}
+	
+	// Update is called once per frame
+	//void Update () {	
+	//}
+
+}
