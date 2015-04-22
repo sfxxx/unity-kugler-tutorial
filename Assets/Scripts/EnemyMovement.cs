7c3c68ab9a3e50f9344aa94e673402f4f2a56d76
sfx <stefanfranke@stefanfranke.com>
2015-04-22 15:50:34 +0200

    Added enemy creation and movement. Blue balls are created with a right click and fall down. Ball created means instantiating a prefab programmatically.

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	//void Start () {
+	//}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position += Vector3.down * this.speed * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+
+	// Use this for initialization
+	//void Start() {
+	//}
+	
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetMouseButtonUp(1)) {
+			Vector3 v = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			v.z = 2;
+			GameObject.Instantiate(this.enemyPrefab, v, Quaternion.identity);
+		}
+	}
+}
