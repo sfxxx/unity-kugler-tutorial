efc624f1af96b998c81a0c717660bc1e21c42e80
sfx <stefanfranke@stefanfranke.com>
2015-04-22 23:00:43 +0200

    End of part 2 of tutorial. Enemy instances now are destroyed when leaving the screen. Added health counter which decrements every time a ball vanishes.

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -12,5 +12,13 @@ public class EnemyMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		this.transform.position += Vector3.down * this.speed * Time.deltaTime;
+
+		if (!this.GetComponent<Renderer>().isVisible) {
+			GameObject system = GameObject.Find("MySystem");
+			if (system != null) {
+				system.SendMessage("DamagePlayer", SendMessageOptions.DontRequireReceiver);
+			}
+			GameObject.Destroy(this.gameObject);
+		}
 	}
 }
---
+++ Assets/Scripts/GameInfo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameInfo : MonoBehaviour {
+
+	public int health;
+
+	public void DamagePlayer() {
+		this.health -= 1;
+		if (health <= 0) {
+			// game over - navigate to retry screen
+			Debug.Log("Game Over");
+		}
+	}
+}
