3169ba54d51db3913e025e730d48293ffe5d3f89
sfx <stefanfranke@stefanfranke.com>
2015-04-23 23:37:12 +0200

    End of part 4 of tutorial. Enemies appear own their own at random horizontal positions at the top of the screen, and drop much faster.

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class EnemyMovement : MonoBehaviour {
 
 	public float speed;
+	private bool rendererWasVisible;
 
 	// Use this for initialization
 	//void Start () {
@@ -13,13 +14,17 @@ public class EnemyMovement : MonoBehaviour {
 	void Update () {
 		this.transform.position += Vector3.down * this.speed * Time.deltaTime;
 
-		if (!this.GetComponent<Renderer>().isVisible) {
+		if (!this.GetComponent<Renderer>().isVisible && this.rendererWasVisible) {
 			// Alternatively we could have called GetComponent on the system gameobject to get the GameInfo component and called the method directly
 			GameObject mySystem = GameObject.Find("MySystem");
 			if (mySystem != null) {
 				mySystem.SendMessage("DamagePlayer", SendMessageOptions.DontRequireReceiver);
 			}
 			GameObject.Destroy(this.gameObject);
+		}
+
+		if (!this.GetComponent<Renderer>().isVisible) {
+			this.rendererWasVisible = true;
 		}
 	}
 }
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class EnemySpawner : MonoBehaviour {
 
+	private int yOffset = 0;
+	// offset to put distance between enemy and edge of the screen
+	private int xOffset = 32;
+
 	public GameObject enemyPrefab;
 
 	// Use this for initialization
@@ -11,10 +15,33 @@ public class EnemySpawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update() {
-		if (Input.GetMouseButtonUp(1)) {
+		// debug only
+		/*if (Input.GetMouseButtonUp(1)) {
 			Vector3 v = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			v.z = 2;
 			GameObject.Instantiate(this.enemyPrefab, v, Quaternion.identity);
-		}
+		}*/
+	}
+
+	private void OnEnable() {
+		Debug.Log("Enable");
+		this.InvokeRepeating("Spawn", 1, 1);
+	}
+
+	private void OnDisable() {
+		Debug.Log("Disable");
+		this.CancelInvoke("Spawn");
+	}
+
+	private void Spawn() {
+		Debug.Log("Spawn");
+		float x = UnityEngine.Random.Range(xOffset, Screen.width-xOffset);
+
+		Vector3 startPos = new Vector3(x, Screen.height + yOffset, 2);
+		Debug.Log("untransformed: " + startPos);
+		Vector3 pos = Camera.main.ScreenToWorldPoint(startPos);
+		Debug.Log("transformed: " + pos);
+
+		GameObject.Instantiate(this.enemyPrefab, pos, Quaternion.identity);
 	}
 }
