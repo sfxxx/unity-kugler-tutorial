6a1b2665a4dce181d6c59eb54a7f562412607541
sfx <stefanfranke@stefanfranke.com>
2015-04-27 23:53:43 +0200

    In part 7 of tutorial. Now playing back hit and miss sounds.

--- Assets/Scripts/GameInfo.cs
+++ Assets/Scripts/GameInfo.cs
@@ -11,9 +11,16 @@ public class GameInfo : MonoBehaviour {
 	public Text healthText;
 	public Text scoreText;
 
+	public AudioSource enemyKilledAudio;
+	public AudioSource playerHitAudio;
+
 	public void DamagePlayer() {
 		this.currentHealth -= 1;
 		this.healthText.text = this.currentHealth.ToString();
+
+		if (this.playerHitAudio != null) {
+			this.playerHitAudio.Play();
+		}
 
 		if (currentHealth == 0) {
 			// game over - navigate to retry screen
@@ -35,5 +42,9 @@ public class GameInfo : MonoBehaviour {
 	public void IncrementScore() {
 		this.score += 1;
 		this.scoreText.text = this.score.ToString();
+
+		if (this.enemyKilledAudio != null) {
+			this.enemyKilledAudio.Play();
+		}
 	}
 }
